f8192932e2608ebaf6eba9ba546f75c9e0bc702a
stamp <stamp@stamp.se>
2011-11-06 19:01:32 +0100

    Made a base for the controlpanel and added the xbee manual

---
+++ usr/share/collision/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">  
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="chrome=1">
+        <meta http-equiv="Expires" content="Tue, 01 Jan 1980 1:00:00 GMT">
+        <meta http-equiv="Pragma" content="no-cache"> 
+        <title>Collision - Lasergame made awsome!</title>
+
+        <script type="text/javascript" src="mootools-core-1.3-full-compat-yc.js"></script>
+
+        <link href="css/base.css" rel="stylesheet" />
+    </head>
+    <body>
+        <div id="right">
+            <div class="status box">
+                <h2>System status</h2>
+                <ul>
+                    <li id="zbif"><div></div>ZigBee interface</li>
+                    <li id="zbcoord"><div></div>ZigBee coordinator</li>
+                    <li id="ctrl"><div></div>Game controller</li>
+                </ul>
+            </div>
+            <div class="network box">
+                <h2>Network
+                <input type="button" value="Discover">
+                </h2>
+            </div>
+        </div>
+        <div id="left">
+            <div class="game box">
+                <h2>Game</h2>
+                
+            </div>
+            <div class="players box">
+                <h2>Players</h2>
+
+            </div>
+        </div>
+    </body>
+</html>
+
