ce8003897fffd616c2475f2c49ed81a320a33d61
Jonas Falck <jonaz@Jonas-Falcks-MacBook-Pro.local>
2011-11-08 21:38:26 +0100

    added test arduino program for receive and send.

---
+++ usr/lib/collision/test/test.cpp
@@ -0,0 +1,45 @@
+#include <XBee.h> 
+#include <NewSoftSerial.h>
+
+int brightness = 0;    // how bright the LED is
+int fadeAmount = 5;    // how many points to fade the LED by
+
+XBee xbee = XBee();
+XBeeResponse response = XBeeResponse();
+Rx64Response rx64 = Rx64Response();
+uint8_t data = 0;
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(11, OUTPUT);
+  xbee.begin(9600);
+  analogWrite(11, 128);    
+}
+
+void loop()
+{
+    xbee.readPacket();
+    
+    if (xbee.getResponse().isAvailable()) {
+      // got something
+     
+      
+      
+      
+      if (xbee.getResponse().getApiId() == 0x90) {
+        // got a rx packet
+        xbee.getResponse().getRx64Response(rx64);
+        data = rx64.getData(1);
+        analogWrite(11, data);
+        
+        uint8_t payload[] = {  rx64.getData(0), rx64.getData(1), rx64.getData(2), rx64.getDataLength(),'l','l','o' };
+        // Specify the address of the remote XBee (this is the SH + SL)
+        XBeeAddress64 addr64 = XBeeAddress64(0x0013a200, 0x4069839a);
+        // Create a TX Request
+        ZBTxRequest zbTx = ZBTxRequest(addr64, payload, sizeof(payload));
+        xbee.send(zbTx);
+
+      }
+    }
+       
+}
